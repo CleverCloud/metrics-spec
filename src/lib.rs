8967c8d14377ccbc08bdaefdcc7a2f9c89084ebb
Clement Delafargue <clement@delafargue.name>
2018-01-23 12:09:35 +0100

    Add data model

---
+++ src/lib.rs
@@ -0,0 +1,70 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
+use std::collections::HashMap;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Range(pub i32, pub i32);
+#[derive(Debug, Deserialize, Serialize)]
+pub enum Axis {
+    Y1,
+    Y2,
+}
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AxisRange(pub Range, pub Range);
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum AggOp {
+    Sum,
+    Mean,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Aggregate(pub AggOp, pub Option<String>);
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum Transform {
+    Rate,
+    Mul(i32),
+    Div(i32),
+    Add(i32),
+    Sub(i32),
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum Display {
+    Stack,
+    Invert,
+    Percent,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Metric {
+    pub name:      String,
+    pub selector:  String,
+    pub aggregate: Vec<Aggregate>,
+    pub transform: Vec<Transform>,
+    pub display:   Vec<Display>,
+    pub axis:      Axis,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Group {
+    pub name:       String,
+    pub range:      Option<Range>,
+    pub axis_range: Option<AxisRange>,
+    pub metrics:    Vec<Metric>,
+}
+#[derive(Debug, Deserialize, Serialize)]
+pub enum Collector {
+    Prometheus(String),
+    Statsd(String),
+    Telegraf,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Metrics {
+    pub collect: Collector,
+    pub groups:  HashMap<String, Group>,
+}
